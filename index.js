44abd11c9a1e3d39971abbf6a84342b1e9954af2
Sofia Kalpin <sofia.kalpin@khk.ee>
2022-01-30 18:55:21 +0200

    “post request use”

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
-// add public directory
-app.use(express.static('public'));
-// add views directory path
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set('views', path.join(__dirname, 'views'));
-// add views template engine
 app.set('view engine', 'ejs');
 
-app.get('/questions', (req, res)=> {
-
-    let questions = [
-        {title: "What is Node.js?", user : "Kadi", votes : "10"},
-        {title: "What is Express.js?", user : "Mike", votes : "8"}
-    ]
-
-    res.render('index', {questions:questions})
+app.get('/login', (req, res)=> {
+    res.render('login');
 });
+
+app.post('/login', (req, res)=> {
+let username = req.body.username;
+let password = req.body.password;
+
+if(username === 'user' && password === 'qwerty'){
+    res.redirect('/dashboard')
+}
+});
+
+app.get('/dashboard', (req, res)=> {
+    res.render('dashboard');
+})
 
 app.listen(port, ()=>{
     console.log('Server started on http://localhost:3000');
