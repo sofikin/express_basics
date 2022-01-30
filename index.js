8277b629ae0893ce116bbbb5f6aaad17e093977c
Sofia Kalpin <sofia.kalpin@khk.ee>
2022-01-30 17:08:56 +0200

    “templating directory setup”

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 // add public directory
 app.use(express.static('public'));
+// add views directory path
+app.set('views', path.join(__dirname, 'views'));
+// add views template engine
+app.set('view engine', 'ejs');
 
 app.get('/user/:username', (req, res)=> {
     // get parameter data from address row
